cf6d1a9ad044fff2a758ff363c586b06c4042eed
JuliansRecent <juliansrecent@gmail.com>
2023-03-10 21:56:37 -0800

    Module complete. Create a terminal based application that asks for a number in a loop and squares any positive number given, exits on the input 0, and continues the loop on with any negative input.

--- src/main/java/OnlyPositives.java
+++ src/main/java/OnlyPositives.java
@@ -6,5 +6,24 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // prompt the user for a number in a loop
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            // if the number given is equal to 0, exit the loop
+            if (number == 0) {
+                break;
+
+            // if the number given is negative, continue the loop
+            } else if (number < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+
+            // if the number given is positive, print the number to the power of two
+            } else {
+                System.out.println(number * number);
+            }
+        }
     }
 }
